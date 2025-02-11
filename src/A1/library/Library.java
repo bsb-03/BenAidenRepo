6397c5805d8590341278e8a63f630ff39077b0c0
Aiden Harrison <asharrison97@gmail.com>
2025-02-11 17:35:35 -0600

    Added the addBook method, which is designed to allow users to add books to their libraries.

--- src/A1/library/Library.java
+++ src/A1/library/Library.java
@@ -3,4 +3,18 @@ package A1.library;
 public class Library {
 	private int[5] Book;
 	private int count;
+	
+	public boolean addBook(Book book){
+		int i=0;
+		for(i=0; i<=5;i++)
+		{
+			if(Book[i] == null)
+				break;
+		}
+		if(i==5)
+			System.out.println("Your library is full!");
+		Book[i] = new Book;
+		count++;
+	}
+	
 }
