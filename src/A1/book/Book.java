91cc178d7fa134add2d250fd1ee9e3b73765ddc6
Aiden Harrison <asharrison97@gmail.com>
2025-02-11 17:00:11 -0600

    Generated getters and setters for private attributes within the Book class.

--- src/A1/book/Book.java
+++ src/A1/book/Book.java
@@ -17,4 +17,35 @@ public class Book {
 		return newBook;
 	}
 	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 }
