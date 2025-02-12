745aef4aab79338118f12abf61a23b08f40a8f21
asharrison <asharrison97@gmail.com>
2025-02-12 17:33:19 -0600

    Added Javadoc for the Library class.

--- src/A1/library/Library.java
+++ src/A1/library/Library.java
@@ -1,12 +1,15 @@
 package A1.library;
 import A1.book.Book;
 
+/** 
+ * Library class. Contains an array of objects Book and int count.
+ */
 public class Library {
 	private Book[] bookArray = new Book[5];
 	private int count = 0;
 	
-	/** public boolean addBook() - This method adds a book object to a library.
-	 * 
+	/** 
+	 * public boolean addBook() - This method adds a book object to a library.
 	 * @param book - This is the book object to be added to the library
 	 * @return
 	 */
@@ -26,8 +29,8 @@ public class Library {
 		return true;
 	}
 
-	/** public boolean removeBook() - This method removes a book object from a library.
-	 * 
+	/** 
+	 * public boolean removeBook() - This method removes a book object from a library.
 	 * @param book - This is the book object to be removed from the library
 	 * @return
 	 */
@@ -45,8 +48,8 @@ public class Library {
 		return false;
 	}
 	
-	/** public Book searchByISBN() - This method searches a library for a book with an ISBN code that matches the input.
-	 * 
+	/** 
+	 * public Book searchByISBN() - This method searches a library for a book with an ISBN code that matches the input.
 	 * @param ISBN - This is the input ISBN code that is to be searched for within the library.
 	 * @return
 	 */
@@ -68,8 +71,8 @@ public class Library {
 		return null;
 	}
 	
-	/** public void displayBooks() - This method displays all of the book objects in a library
-	 * 
+	/** 
+	 * public void displayBooks() - This method displays all of the book objects in a library
 	 * @return
 	 */
 	public void displayBooks() {
@@ -81,16 +84,16 @@ public class Library {
 		}
 	}
 
-	/** public int getCount() - This is the getter method for the count attribute within the library class.
-	 * 
+	/** 
+	 * public int getCount() - This is the getter method for the count attribute within the library class.
 	 * @return
 	 */
 	public int getCount() {
 		return count;
 	}
 
-	/** public int setCount() - This is the setter method for the count attribute within the library class.
-	 * 
+	/** 
+	 * public int setCount() - This is the setter method for the count attribute within the library class.
 	 * @param int count - This is the amount you wish to set the count to.
 	 * @return
 	 */
